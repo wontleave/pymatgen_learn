7926a4a354c19b9374a481098c3db3e7f58a13ab
wontleave <wontleave@gmail.com>
2021-12-14 11:49:45 +0800

    Initial

---
+++ visualize_struct.py
@@ -0,0 +1,7 @@
+from pymatgen.vis.structure_vtk import StructureVis
+from pymatgen.io.vasp import Poscar
+from os import path
+
+root_path = r"E:\vBoxShared\ZXY\enlarged_2x2x3\HSE06-u0p13"
+poscar_path = path.join(root_path, "POSCAR")
+
